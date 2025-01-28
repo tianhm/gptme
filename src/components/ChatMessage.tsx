f16c11937b0599ab90baeee5f54ed73983a4cbd3
Erik Bjäreholt <erik@bjareho.lt>
2025-01-28 19:02:32 +0100

    fix: improved styling

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -165,19 +165,28 @@ export const ChatMessage: FC<Props> = ({ message }) => {
   const isSuccess = message.content.startsWith("Patch successfully");
 
   const avatarClasses = `hidden md:flex mt-0.5 flex-shrink-0 w-8 h-8 rounded-full items-center justify-center absolute ${
-    isAssistant
-      ? "bg-gptme-600 text-white left-0"
-      : isSystem
-          ? (isError ? "bg-red-800 text-red-100" : (isSuccess ? "bg-green-800 text-green-100" : "bg-slate-500 text-white left-0"))
-      : "bg-blue-600 text-white right-0"
+      isUser
+      ? "bg-blue-600 text-white right-0"
+      : isAssistant
+          ? "bg-gptme-600 text-white left-0"
+          : (isError
+            ? "bg-red-800 text-red-100"
+            : (isSuccess
+                ? "bg-green-800 text-green-100"
+                : "bg-slate-500 text-white left-0")
+            )
   }`;
 
   const messageClasses = `rounded-lg px-3 py-1.5 ${
-    isAssistant
-      ? "bg-card"
-      : isUser
-          ? "bg-[#EAF4FF] text-black dark:bg-[#2A3441] dark:text-white"
-          : (isError ? "bg-[#FFDDDD] dark:bg-[#440000] text-red-500" : (isSuccess ? "bg-green-100 text-green-900 dark:bg-green-900 dark:text-green-200" : "bg-muted"))
+    isUser
+      ? "bg-[#EAF4FF] text-black dark:bg-[#2A3441] dark:text-white"
+      : isAssistant
+          ? "bg-card"
+          : isError
+              ? "bg-[#FFDDDD] dark:bg-[#440000] text-red-500"
+              : (isSuccess
+                  ? "bg-green-100 text-green-900 dark:bg-green-900 dark:text-green-200"
+                  : "bg-card")
   }`;
 
   return (
