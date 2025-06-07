9c944e62459c5f08deb0ea5ca5ee925ef46e59d3
Erik Bjäreholt <erik@bjareho.lt>
2025-06-07 13:21:24 +0200

    fix: added support for TEMPERATURE and TOP_P environment variables

--- gptme/constants.py
+++ gptme/constants.py
@@ -2,12 +2,13 @@
 Constants
 """
 
+import os
+
 # Optimized for code
 # Discussion here: https://community.openai.com/t/cheat-sheet-mastering-temperature-and-top-p-in-chatgpt-api-a-few-tips-and-tricks-on-controlling-the-creativity-deterministic-output-of-prompt-responses/172683
-# TODO: make these configurable
-
-TEMPERATURE = 0
-TOP_P = 0.1
+# NOTE: getting the environment variables like this ignores if they are set in the gptme config
+TEMPERATURE = float(os.getenv("TEMPERATURE", "0"))
+TOP_P = float(os.getenv("TOP_P", "0.1"))
 
 # separator for multiple rounds of prompts on the command line
 # demarcates the end of the user's prompt, and start of the assistant's response
